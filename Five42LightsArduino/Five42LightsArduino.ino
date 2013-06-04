bc5fbf38b41adbce668178426d456440e643bd96
Thiago Hersan <thiago.hersan+github@gmail.com>
2013-06-04 00:37:39 -0700

    adds serial interface to control RGB values

--- Five42LightsArduino/Five42LightsArduino.ino
+++ Five42LightsArduino/Five42LightsArduino.ino
@@ -5,6 +5,8 @@
 #define GREEN 10
 #define BLUE  11
 
+#define DEBUG 0
+
 template <typename T, size_t N>
 inline size_t sizeOf(T(&array)[N]) {
   return N;
@@ -14,6 +16,7 @@ unsigned int allPins[] = {
   WHITE_RIGHT, WHITE_LEFT, RED, GREEN, BLUE};
 
 int rgbv,wv;
+byte RGB[3] = {0,0,0};
 long rgbt,wt;
 
 void setup(){
@@ -29,27 +32,45 @@ void setup(){
 }
 
 void loop(){
-  if(Serial.available() > 2){
+  if(Serial.available() > 1){
+    char c = Serial.read();
+    int b = Serial.read();
+    while(Serial.available()){
+      Serial.read();
+    }
+    if(c == 'R'){
+      RGB[0] = constrain(map(b&0xff,48,57,0,255),0,255);
+    }
+    else if(c == 'G'){
+      RGB[1] = constrain(map(b&0xff,48,57,0,255),0,255);
+    }
+    else if(c == 'B'){
+      RGB[2] = constrain(map(b&0xff,48,57,0,255),0,255);
+    }
   }
-  
+
   if(millis()-rgbt > 500){
-    rgbv = (rgbv+1)%8;
-    digitalWrite(RED, (rgbv>>0)&0x1);
-    digitalWrite(GREEN, (rgbv>>1)&0x1);
-    digitalWrite(BLUE, (rgbv>>2)&0x1);
+    if(DEBUG){
+      rgbv = (rgbv+1)%8;
+      digitalWrite(RED, (rgbv>>0)&0x1);
+      digitalWrite(GREEN, (rgbv>>1)&0x1);
+      digitalWrite(BLUE, (rgbv>>2)&0x1);
+    }
+    else{
+      analogWrite(RED, RGB[0]);
+      analogWrite(GREEN, RGB[1]);
+      analogWrite(BLUE, RGB[2]);
+    }
     rgbt = millis();
   }
 
   if(millis()-wt > 8){
     wv += 8;
     wv = (wv>255)?(-255):(wv);    
-    analogWrite(WHITE_LEFT, abs(wv));
-    analogWrite(WHITE_RIGHT, abs(wv));
+    //analogWrite(WHITE_LEFT, abs(wv));
+    //analogWrite(WHITE_RIGHT, abs(wv));
     wt = millis();
   }
 
 }
-
-
-
 
