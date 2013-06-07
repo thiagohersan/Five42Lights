3d4399c78b4aa420ccdc4d53c50b7aa88201d0f5
Thiago Hersan <thiago.hersan+github@gmail.com>
2013-06-07 13:10:41 -0700

    makes debug mode dynamic and serial receive more flexible

--- Five42LightsArduino/Five42LightsArduino.ino
+++ Five42LightsArduino/Five42LightsArduino.ino
@@ -5,8 +5,6 @@
 #define GREEN 10
 #define BLUE  11
 
-#define DEBUG 0
-
 template <typename T, size_t N>
 inline size_t sizeOf(T(&array)[N]) {
   return N;
@@ -18,9 +16,11 @@ unsigned int allPins[] = {
 int rgbv,wv;
 byte RGB[3] = {0,0,0};
 long rgbt,wt;
+boolean debugMode;
 
 void setup(){
   Serial.begin(57600);
+  debugMode = true;
 
   // set pin modes
   for(unsigned int i=0; i<sizeOf(allPins); ++i){
@@ -38,19 +38,25 @@ void loop(){
     while(Serial.available()){
       Serial.read();
     }
-    if(c == 'R'){
-      RGB[0] = constrain(map(b&0xff,48,57,0,255),0,255);
+    if((c == 'R') || (c == 'r')){
+      debugMode = false;
+      RGB[0] = b&0xff;
     }
-    else if(c == 'G'){
-      RGB[1] = constrain(map(b&0xff,48,57,0,255),0,255);
+    else if((c == 'G') || (c == 'g')){
+      debugMode = false;
+      RGB[1] = b&0xff;
     }
-    else if(c == 'B'){
-      RGB[2] = constrain(map(b&0xff,48,57,0,255),0,255);
+    else if((c == 'B') || (c == 'b')){
+      debugMode = false;
+      RGB[2] = b&0xff;
+    }
+    else if((c == 'D') || (c == 'd')){
+      debugMode = true;
     }
   }
 
   if(millis()-rgbt > 500){
-    if(DEBUG){
+    if(debugMode){
       rgbv = (rgbv+1)%8;
       digitalWrite(RED, (rgbv>>0)&0x1);
       digitalWrite(GREEN, (rgbv>>1)&0x1);
